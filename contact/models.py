a5b245d9d067786195144af3bd8fe779d1541851
Alex Wallace <alexwallax@gmail.com>
2025-03-30 19:02:52 -0300

    Imagens

--- contact/models.py
+++ contact/models.py
@@ -5,17 +5,18 @@ from django.utils import timezone
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 
-# Depois
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True) # blank=True -> não obrigatorieddade 
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     create_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
