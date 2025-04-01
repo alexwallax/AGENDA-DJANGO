483aaa33e7a53007c6b5402a231abcfdbd07936c
Alex Wallace <alexwallax@gmail.com>
2025-04-01 19:01:42 -0300

    adicionando categorias 02

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
@@ -9,6 +10,10 @@ from django.utils import timezone
 # picture (imagem)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -25,6 +30,11 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True 
+    )
+    owner = models.ForeignKey(
+        User, 
         on_delete=models.SET_NULL, 
         blank=True, null=True 
     )
