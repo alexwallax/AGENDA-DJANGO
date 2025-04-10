8cdec2c9bcd17ef548ec589581e8d5c73844d0dd
Alex Wallace <alexwallax@gmail.com>
2025-04-10 11:02:32 -0300

    paginação

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q
 from contact.models import Contact
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10) 
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -36,8 +41,12 @@ def search(request):
         )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10) 
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number) 
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ', 
         'search_value': search_value,
     }
