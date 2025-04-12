79af800be110947cf806c5916afebbe730c93402
Alex Wallace <alexwallax@gmail.com>
2025-04-12 09:46:22 -0300

    Form 01

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,34 @@
+from django import forms
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+
 
 def create(request):
-    context = {
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+        
+
+    context = {
+        'form': ContactForm()
     }
 
     return render(
