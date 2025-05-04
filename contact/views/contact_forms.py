361a12e6a428ae16728442467129da683e9a39b6
Alex Wallace <alexwallax@gmail.com>
2025-05-04 09:36:06 -0300

    delete

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from contact.forms import ContactForm
@@ -77,4 +77,25 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
-    
+
+
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+ 
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+ 
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
+    )
